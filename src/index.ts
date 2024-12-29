7b58697d500ed2fb6846a6724ef61392c1d9f35f
dmitryprogrammer <dpischalka@gmail.com>
2024-12-29 12:35:02 +0300

    create-crud: rewrited actions by commander.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import {Command} from "commander";
 import {actions} from "./actions";
 
-actions.add("tesst 1");
+const program = new Command();
+
+program
+  .name("file-manager")
+  .description("CLI to manage files")
+  .version("1.0.0");
+
+program.command("add <taskTitle>").description("Add task").action(actions.add);
+program.command("clear-tasks").action(actions.clearTasks);
+
+program.parse(process.argv);
