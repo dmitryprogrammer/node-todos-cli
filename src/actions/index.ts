878e33740d643a681012720f536638ac86ae20b2
dmitryprogrammer <dpischalka@gmail.com>
2025-01-05 00:08:27 +0300

    feat(create-crud): export actions.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,15 @@
-import {add, clearTasks, getTasksList} from "./add.action";
+import {add} from "./add.action";
+import {clearTasks} from "./clear.action";
+import {deleteTask} from "./delete.action";
+import { list } from "./list.action";
+import {update} from "./update.action";
 
 export type ACTION_NAMES = "add" | "get" | "delete" | "list";
 
 export const actions = {
   add,
-  delete,
-  getTasksList,
+  delete: deleteTask,
+  list,
+  update,
   clearTasks,
 };
