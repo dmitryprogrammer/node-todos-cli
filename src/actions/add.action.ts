fe38619754a203610876774cc5cdab093edd9848
dmitryprogrammer <dpischalka@gmail.com>
2025-01-05 00:08:08 +0300

    feat(create-crud): add action finished.

--- src/actions/add.action.ts
+++ src/actions/add.action.ts
@@ -1,6 +1,5 @@
-import exp = require("constants");
 import {dbConnector} from "../db";
-import {Task, Tasks} from "../db/connector";
+import {Task} from "../db/connector";
 import {successLog} from "../utils/log.util";
 
 export function add(title: Task["title"]): void {
@@ -12,23 +11,4 @@ export function add(title: Task["title"]): void {
   dbConnector.writeTaskToDb(title).then(() => {
     successLog(`Task ${title} was added successfully`);
   });
-}
-
-export deleteTask(title: Task["title"]): void {
-  if (!title) {
-    throw new Error("Need title for task");
-    return;
-  }
-
-  dbConnector.deleteTaskFromDb(title).then(() => {
-    successLog(`Task ${title} was deleted successfully`);
-  });
-}
-
-export function getTasksList(): Promise<Tasks> {
-  return dbConnector.getTasksList();
-}
-
-export function clearTasks(): Promise<void> {
-  return dbConnector.clearTasks();
 }
