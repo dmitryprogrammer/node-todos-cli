34e755479cd18bbe549658a8575f08719e9ec739
dmitryprogrammer <dpischalka@gmail.com>
2025-01-05 00:08:15 +0300

    feat(create-crud): list action.

---
+++ src/actions/list.action.ts
@@ -0,0 +1,16 @@
+import {dbConnector} from "../db";
+import {Tasks} from "../db/connector";
+import {errorLog, successLog} from "../utils/log.util";
+
+export function list(): void {
+  dbConnector
+    .getTasksList()
+    .then((tasks: Tasks) => {
+      for (const task of tasks) {
+        successLog(`Task id: ${task.id}.\nTask title: ${task.title}\n`);
+      }
+    })
+    .catch(() => {
+      errorLog("List tasks error");
+    });
+}
