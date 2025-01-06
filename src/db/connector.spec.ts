14b83bf28c7e7ca81043dd4cabf0a8bcd6822893
dmitryprogrammer <dpischalka@gmail.com>
2025-01-06 10:31:39 +0300

    feat(specs): covered getTasksList method.

--- src/db/connector.spec.ts
+++ src/db/connector.spec.ts
@@ -2,6 +2,8 @@ import {Connector, Tasks} from "./connector";
 
 describe("Connector specs", () => {
   const connector = new Connector();
+  const taskTitle = "Spec task title";
+  const secondTaskTitle = "Second spec task title";
 
   it("should be cleared", async () => {
     await connector.clearTasks();
@@ -10,9 +12,20 @@ describe("Connector specs", () => {
   });
 
   it("should write task to db", async () => {
-    const taskTitle = "Spec task title";
     await connector.writeTaskToDb(taskTitle);
+    await connector.writeTaskToDb(secondTaskTitle);
     const tasks: Tasks = await connector.getTasksList();
-    expect(tasks).toEqual([{title: taskTitle, id: 1}]);
+    expect(tasks).toEqual([
+      {title: taskTitle, id: 1},
+      {title: secondTaskTitle, id: 2},
+    ]);
+  });
+
+  it("should get tasks list", async () => {
+    const tasks: Tasks = await connector.getTasksList();
+    expect(tasks).toEqual([
+      {title: taskTitle, id: 1},
+      {title: secondTaskTitle, id: 2},
+    ]);
   });
 });
