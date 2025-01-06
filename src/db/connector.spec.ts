76b5cd3b20548a4563cb62da85ccdf8b9d3e2d54
dmitryprogrammer <dpischalka@gmail.com>
2025-01-06 10:28:59 +0300

    feat(specs): finished writeTaskToDb method test coverage.

--- src/db/connector.spec.ts
+++ src/db/connector.spec.ts
@@ -8,4 +8,11 @@ describe("Connector specs", () => {
     const tasks: Tasks = await connector.getTasksList();
     expect(tasks).toEqual(null);
   });
+
+  it("should write task to db", async () => {
+    const taskTitle = "Spec task title";
+    await connector.writeTaskToDb(taskTitle);
+    const tasks: Tasks = await connector.getTasksList();
+    expect(tasks).toEqual([{title: taskTitle, id: 1}]);
+  });
 });
