8a299f80b9314c0cb3842a3ed5654d82af8f3814
dmitryprogrammer <dpischalka@gmail.com>
2025-01-12 20:01:34 +0300

    feat(specs): covered delete action by test.

--- src/actions/delete/delete.action.ts
+++ src/actions/delete/delete.action.ts
@@ -2,13 +2,13 @@ import {dbConnector} from "../../db";
 import {Task} from "../../db/connector";
 import {successLog} from "../../utils/log.util";
 
-export function deleteTask(title: Task["title"]): void {
+export function deleteTask(title: Task["title"]): Promise<void> {
   if (!title) {
     throw new Error("Need title for task");
     return;
   }
 
-  dbConnector.deleteTaskFromDb(title).then(() => {
+  return dbConnector.deleteTaskFromDb(title).then(() => {
     successLog(`Task ${title} was deleted successfully`);
   });
 }
