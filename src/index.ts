a7ac8ecab9f49c1925001e39d8b3b8f379716e61
dmitryprogrammer <dpischalka@gmail.com>
2025-01-31 14:00:36 +0300

    feat(main): format.

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,15 @@ program
   .version("1.0.0");
 
 program.command("add <taskTitle>").description("Add task").action(actions.add);
-program.command("delete <taskTitle>").description("Delete task").action(actions.delete);
-program.command("update <taskTitle> <newTaskTitle>").description("Update task").action(actions.update);
+program
+  .command("delete <taskTitle>")
+  .description("Delete task")
+  .action(actions.delete);
+program
+  .command("update <taskTitle> <newTaskTitle>")
+  .description("Update task")
+  .action(actions.update);
 program.command("list").description("List tasks").action(actions.list);
 program.command("clear-tasks").action(actions.clearTasks);
 
 program.parse(process.argv);
-
